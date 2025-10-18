0da6803fe950a7b614384ecdc1a3d38f884d7f5c
jojanajeangarabillo <155555493+jojanajeangarabillo@users.noreply.github.com>
2025-10-18 17:27:09 +0800

    Add files via upload

---
+++ Admin-Dashboard.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <!--For icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+</head>
+<body class="admin-dashboard">
+    <header class="admin-header">
+        <h1 class="admin-header-text">Human Resource</h1>
+    </header>
+
+    <aside class="admin-sidebar">
+        <div class="sidebar-logo">
+            <img src="Images/Screenshot 2024-12-31 233611.png" alt="happy" >
+        </div>
+        <nav class="sidebar-nav">
+            <!--Primary top nav-->
+            <ul class="primary-top-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-grip"></i>
+                        <span class="nav-label">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-user-group"></i>
+                        <span class="nav-label">Employees</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-user-group"></i>
+                        <span class="nav-label">Applicants</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-code-pull-request"></i>
+                        <span class="nav-label">Requests</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-folder"></i>
+                        <span class="nav-label">Job Post</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-chart-simple"></i>
+                        <span class="nav-label">Reports</span>
+                    </a>
+                </li>
+                
+                
+            </ul>
+            <!--Secondary bottom nav-->
+            <ul class="secondary-buttom-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-gear"></i>
+                        <span class="nav-label">Settings</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                        <span class="nav-label">Logout</span>
+                    </a>
+                </li>
+            </ul>
+
+            
+        </nav>
+
+    </aside>
+    <main class="admin-main">
+        <div class="banner-card"></div>
+        <div class="employee-card">
+            <i class="fa-solid fa-user-group"></i>
+            <label for="employees" class="employee-label">Employees</label>
+            <h3 class="employee-count">0</h3>
+        </div>
+        <div class="applicant-card"></div>
+        <div class="request-card"></div>
+        <div class="hiring-card"></div>
+        <div class="recent-job-post-card"></div>
+        <div class="newly-hired-card"></div>
+
+    </main>
+    
+</body>
+</html>
---
+++ Login-Admin.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <!--For icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="login-body">
+    <!--Topbar-->
+    <nav class="top-bar">
+        <div class="logo-header">
+            <img src="Images/Screenshot 2024-12-31 233611.png" alt="Happy Picture" >
+            <div class="top-bar-text">
+                <h1>Hospital</h1>
+                <h4>Human Resources</h4>
+            </div>
+        </div>
+    </nav>
+
+    <!--Login Field-->
+    <section class="main-content">
+        <section class="login-section">
+            <div class="login-input">
+                <div class="login-email">
+                    <label>Email</label>
+                    <i class="fa-solid fa-envelope"></i>
+                    <input type="email" name="email" id="email" placeholder="Enter your Email">
+                </div>
+                <div class="login-password">
+                    <label>Password</label>
+                    <i class="fa-solid fa-lock"></i>
+                    <input type="password" name="password" id="password" placeholder="Enter your Password">
+                </div>
+                <div class="show-password">
+                    <input type="checkbox" name="Show password" id="show"> Show password
+                </div>
+                <div class="login-button">
+                    <button><i class="fa-solid fa-right-to-bracket"></i>Sign In</button>
+                </div>
+                <div class="forgot-password">
+                    <a href="Admin-Forgot-Password.html" class="forgot-password-link">Forgot password?</a>
+                </div>
+            </div>
+
+        </section>
+    </section>
+
+</body>
+</html>
