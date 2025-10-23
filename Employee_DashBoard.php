5d9b9907e21d301fc3cfdc0c136b5f5f36283f43
Shane Ella Mae F. Cacho <cacho_shaneellamae@plpasig.edu.ph>
2025-10-24 04:50:45 +0800

    Employee Dashboard and Sidebar

---
+++ Employee_DashBoard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Employee Dashboard</title>
+  <link rel="stylesheet" href="sidebar.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+</head>
+<body>
+ <aside class="sidebar">
+  <img src="images/hospital-logo.png" alt="Hospital Logo">
+
+  <ul class="sidebar-menu">
+    <li><a href="#" class="active"><i class="fa-solid fa-grip"></i> Dashboard</a></li>
+    <li><a href="#"><i class="fa-solid fa-user-group"></i> Salary Slip</a></li>
+    <li><a href="#"><i class="fa-solid fa-code-branch"></i> Requests</a></li>
+    <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
+  </ul>
+</aside>
+
+</body>
+</html>
