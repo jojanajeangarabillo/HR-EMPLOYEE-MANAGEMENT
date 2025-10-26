7f80387a70e67696b811284f197db243b753c5b1
Rhoanne Nicole Antonio <antonio_rhoannenicole@plpasig.edu.ph>
2025-10-26 20:50:12 +0800

    admin dashboard

--- Admin_Dashboard.php
+++ Admin_Dashboard.php
@@ -26,7 +26,7 @@ if ($applicantQuery && $row = $applicantQuery->fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboardt</title>
-    <link rel="stylesheet" href="applicant.css">
+    <link rel="stylesheet" href="admin-sidebar.css">
     <!--For icons-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
